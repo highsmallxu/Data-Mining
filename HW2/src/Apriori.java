822b8624b19f03a73c1c4403f94069ea577980f5
王瑜 <wyhitcs@gmail.com>
2016-11-14 16:26:25 +0100

    separate read from main

--- HW2/src/Apriori.java
+++ HW2/src/Apriori.java
@@ -1,8 +1,5 @@
 import java.io.*;
-import java.util.*;
 import java.lang.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -12,35 +9,22 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.TreeMap;
-import java.util.ArrayList;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
 
 public class Apriori {
 
     public static void main(String[] args) throws Exception {
-        int MSF = 2;
-        double MCONF = 0.6;
-        String data = "src/sdata.dat";
+        int min_sup = 2;
+        double min_conf = 0.6;
+        String data = "sdata.dat";
         String output = "result.txt";
 
         // read data file and init transactions
-        List<Set<String>> trans = new LinkedList<Set<String>>();
-        List<String> list = new ArrayList<>();
-        try (Stream<String> stream = Files.lines(Paths.get(data))) {
-            list = stream
-                    .collect(Collectors.toList());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        for(int i=0; i<list.size(); i++){
-            Set<String> items = new HashSet<String>(Arrays.asList(list.get(i).split(" ")));
-            trans.add(items);
-        }
+        ReadData readDat = new ReadData();
+        List<Set<String>> trans = readDat.read(data);
 
         //find frequent item sets
-        Map<Integer, Set<ItemSet>> rst = findFrequentItemSets(trans, MSF,output);
+        Map<Integer, Set<ItemSet>> rst = findFrequentItemSets(trans, min_sup,output);
 
 
         Map<ItemSet, ItemSet> directMap = new HashMap<ItemSet, ItemSet>();
@@ -49,17 +33,17 @@ public class Apriori {
                 directMap.put(set, set);
         }
         //generate association rule
-        //genRule(rst,directMap,MCONF,output);
+        genRule(rst,directMap,min_conf,output);
     }
 
     //find frequent item set
     static Map<Integer, Set<ItemSet>> findFrequentItemSets(
-            Iterable<Set<String>> transIterable, int MSF, String output) {
+            Iterable<Set<String>> transIterable, int min_sup, String output) {
         Map<Integer, Set<ItemSet>> ret = new TreeMap<Integer, Set<ItemSet>>();
 
         //find 1 item frequent set
         Iterator<Set<String>> it = transIterable.iterator();
-        Set<ItemSet> oneItemSets = findFrequentOneItemSets(it, MSF);
+        Set<ItemSet> oneItemSets = findFrequentOneItemSets(it, min_sup);
         ret.put(1, oneItemSets);
 
         int preItemSetSize = 1;
@@ -83,7 +67,7 @@ public class Apriori {
             //candidate item set which has support larger than min_sup could be frequent item set
             Set<ItemSet> curItemSets = new HashSet<ItemSet>();
             for (ItemSet candidate : candidates)
-                if (candidate.frequence >= MSF)
+                if (candidate.frequence >= min_sup)
                     curItemSets.add(candidate);
             if (!curItemSets.isEmpty())
                 ret.put(curItemSetSize, curItemSets);
@@ -113,8 +97,7 @@ public class Apriori {
     }
 
     //find frequent one item set
-    static Set<ItemSet> findFrequentOneItemSets(Iterator<Set<String>> trans,
-                                                int MSF) {
+    static Set<ItemSet> findFrequentOneItemSets(Iterator<Set<String>> trans, int min_sup) {
 
         //find frequency of items in set
         Map<String, Integer> frequences = new HashMap<String, Integer>();
@@ -132,7 +115,7 @@ public class Apriori {
         for (Entry<String, Integer> entry : frequences.entrySet()) {
             String item = entry.getKey();
             Integer frequence = entry.getValue();
-            if (frequence >= MSF) {
+            if (frequence >= min_sup) {
                 ItemSet set = new ItemSet(new String[] { item });
                 set.frequence = frequence;
                 ret.add(set);
@@ -171,9 +154,34 @@ public class Apriori {
         }
         return ret;
     }
+
     //generate rule
-    static void genRule(Map<Integer, Set<ItemSet>> rst,Map<ItemSet, ItemSet> directMap,double MCONF,String output){
-
-
+    static void genRule(Map<Integer, Set<ItemSet>> rst,Map<ItemSet, ItemSet> directMap,double min_conf,String output){
+        try {
+            PrintWriter writer =  new PrintWriter(new FileWriter(output, true));
+            writer.println("Association Rules:");
+            //System.out.println("Association Rules:");
+            for (Entry<Integer, Set<ItemSet>> entry : rst.entrySet()) {
+                for (ItemSet set : entry.getValue()) {
+                    double cnt1 = directMap.get(set).frequence;
+                    List<ItemSet> subSets = set.listNotEmptySubItemSets();
+                    for (ItemSet subSet : subSets) {
+                        int cnt2 = directMap.get(subSet).frequence;
+                        double conf = cnt1 / cnt2;
+                        if (cnt1 / cnt2 >= min_conf) {
+                            ItemSet remainSet = new ItemSet();
+                            remainSet.addAll(set);
+                            remainSet.removeAll(subSet);
+                            writer.printf("%s => %s, %.2f\n", subSet,remainSet, conf);
+                            //System.out.printf("%s => %s, %.2f\n", subSet, remainSet, conf);
+                        }
+                    }
+                }
+            }
+            writer.close();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
